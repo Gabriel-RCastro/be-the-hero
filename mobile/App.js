ba23cb7e8163d0d1670a005a2aa6cd10b29050c5
Gabriel Rodrigues de Castro <gabriel.rodrigues@edu.unifil.br>
2020-03-29 16:44:40 -0300

    Modificando pack-lock.json do mobile

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+
+import Routes from './src/routes'; //IMPORTANDO AS ROTAS 
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
